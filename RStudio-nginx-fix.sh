93bdd32c7253c09d43f37e4f1ed41d88b6ef79e1
Jonathan Smith <47522338+Jtsmith275@users.noreply.github.com>
2021-01-21 15:45:09 +0000

    Update and rename rstudioshinynginxwrapper.sh to RStudio-nginx-fix.sh

---
+++ RStudio-nginx-fix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# R/RStudio/Shiny-Server/nginx on Ubuntu
+
+# Configure nginx with RStudio-Server and Shiny-Server virtualhosts
+sudo cp default /etc/nginx/sites-enabled/default
+
+# Restart services
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+sudo systemctl restart rstudio-server
+
+#Tell user everything works
+echo "nginx is now hosting a webpage on http://127.0.0.1"
+echo "RStudio Server is now available on http://127.0.0.1:8787 & http://127.0.0.1/rstudio"
+
